f15dedeebb5aaff8339243b55418351686c88a52
YD Dev <132710435+YD-YoshiTv@users.noreply.github.com>
2023-12-13 20:03:39 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'YoshiTv'
+version '1.0'
+discord 'https://discord.gg/v73eQpGp5b'
+name 'Temperature Menu'
+description 'Temperature_Menu'
+
+shared_script {
+    '@es_extended/imports.lua',
+    '@ox_lib/init.lua'
+}
+
+client_script {
+    'client.lua'
+}
